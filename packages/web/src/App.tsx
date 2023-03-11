1b0685092b6b00096cc2c0ec7ceaac7032181a18
Krit <you@example.com>
2023-03-12 01:11:15 +0700

    update mui list item and icon

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,43 +1,53 @@
+import { Button, Card, IconButton } from "@mui/material";
 import ListMenuItem from "./components/ListMenuItem";
 import NavImg from "./components/NavImg";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 const DUM_COVER_IMG =
   "https://img.wongnai.com/p/1920x0/2021/08/14/f6ae0252eb0d44b79553c0dba6e56cfe.jpg";
 const DUM_NAME = "ลืมเคี้ยว";
 
-const testArray = [1, 2, 3];
+const testArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function App() {
   return (
     <>
       <NavImg coverImg={DUM_COVER_IMG} />
-      <div className="global-font" style={{ display: "flex" }}>
-        <div style={{ width: "200px" }}></div>
-        <div>
+
+      <div style={{ paddingTop: "30px" }} />
+
+      <Grid container spacing={1} columns={8} style={{ paddingLeft: "30px" }}>
+        <Grid item xs={1} sx={{ display: { xs: "none", md: "block" } }} />
+        <Grid item xs={7}>
           <div style={{ display: "flex" }}>
-            <div>
-              <h1>{DUM_NAME}</h1>
-            </div>
-            <div
+            <h1 style={{ fontSize: "50px" }}>{DUM_NAME}</h1>
+            <Button
+              variant="contained"
+              disabled
               style={{
-                backgroundColor: "green",
-                paddingTop: "15px",
+                color: "white",
                 marginLeft: "10px",
-                borderRadius: "10px",
-                width: "50px",
+                backgroundColor: "green",
+                borderRadius: "15px",
               }}
             >
               {" "}
               เปิด
-            </div>
+            </Button>
           </div>
-          
-          {testArray.map((x) => {
-            console.log(x);
-            return (<ListMenuItem />);
+
+          <div style={{ paddingTop: "30px" }} />
+        </Grid>
+      </Grid>
+      <Card sx={{ padding:'10px'}}>
+        <div style={{ overflow: "auto", display: "flex" }}>
+          {testArray.map((x ,index) => {
+            // console.log(x);
+            return <ListMenuItem key={index}/>;
           })}
         </div>
-      </div>
+      </Card>
     </>
   );
 }
--- packages/web/src/components/ListMenuItem.tsx
+++ packages/web/src/components/ListMenuItem.tsx
@@ -1,21 +1,82 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 
-const DUM_IMG = 'https://img.freepik.com/free-vector/hand-drawn-flat-design-japan-food-illustration_23-2149281758.jpg'
+const DUM_NAME = "ราเม็ง";
 
+const DUM_DES = `เมนูเส้นของญี่ปุ่น ความหมายโดยรวมจะครอบคลุมทั้งราเมง อุด้ง และโซบะ แต่เส้นราเมงคือเส้นกลมสีเหลือง คล้ายบะหมี่เหลืองของไทย ทำจากแป้งสาลี มีความเหนียวนุ่ม นิยมทานแบบน้ำร้อนๆ
+บางคนอาจสงสัยว่า ราเมง หรือ ราเมน กันแน่ แต่ละร้านสะกดไม่เหมือนกันเลย ความจริงคือจะอ่านแบบไหนก็ได้ เพราะในภาษาญี่ปุ่น เสียงตัวสะกด ん จะอยู่ระหว่างเสียง ง กับ น ของไทย จึงทับศัพท์เป็นภาษาไทยได้ทั้ง 2 แบบ
+`;
+const DUM_IMG =
+  "https://img.freepik.com/free-vector/hand-drawn-flat-design-japan-food-illustration_23-2149281758.jpg";
 
+const hot = true;
+const out = !true;
+
+// const [menuCardStatus,setMenuCardStatus] = useState<boolean>(false);
+
+const menuClick = () => {
+  console.log("menu click");
+  //toggle
+  // setMenuCardStatus(!menuCardStatus)
+};
+
+  
 
 export default function ListMenuItem() {
   return (
     <>
-      <div style={{ display: "flex" }}>
-        <img
-          src= {DUM_IMG}
-          style={{
-            width: "100px",
-            objectFit: "cover",
-          }}
-        />
-        <p>Raman</p>
+    
+      <div style={{ padding: "10px", width: "500px", position: "relative" }}>
+        <CardActionArea onClick={menuClick}>
+          <img
+            height="245"
+            width="245"
+            src={DUM_IMG}
+            style={{ objectFit: "cover" }}
+          />
+          <CardContent>
+            <h1>{DUM_NAME}</h1>
+            <h4>{DUM_DES}</h4>
+          </CardContent>
+        </CardActionArea>
+        <div></div>
+
+        {out ? (
+          <img
+            src="https://www.freeiconspng.com/thumbs/stop-icon/stop-icon-21.png"
+            width="50"
+            height="50"
+            style={{
+              position: "absolute",
+              top: 0,
+              right: 0,
+            }}
+          />
+        ) : null}
+
+        {hot ? (
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/3655/3655418.png"
+            width="50"
+            height="50"
+            style={{
+              position: "absolute",
+              top: 0,
+              right: 0,
+            }}
+          />
+        ) : null}
       </div>
+          
     </>
   );
 }
